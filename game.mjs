d08288247c413b13bd09e413a12e2d61dbe52a49
Arkadiusz Pawlak <arkadiusz.pawlak@goodylabs.com>
2022-06-02 10:36:55 +0200

    basic map, no 2 same users in game, starting game

--- game.mjs
+++ game.mjs
@@ -1,16 +1,43 @@
+import { generateNewGame } from "./utils/gameUtils.mjs";
 import { decodeToken, resolveUser } from "./utils/index.mjs";
 import { io } from "./utils/serverSetup.mjs";
 
+const MAX_PLAYERS = 2;
+
 export function game() {
-  const game = { lobby: [], started: false };
+  const game = {
+    lobby: [],
+    started: false,
+    starting: false,
+    map: generateNewGame(),
+  };
+
+  const emitGameState = () => {
+    io.to("game").emit("gameDetails", { map: game.map });
+  };
+
+  let startingTimeout = null;
+
+  const startGame = () => {
+    game.lobby.forEach((p) => {
+      if (p.ready) {
+        p.socket.join("game");
+      }
+    });
+    game.started = true;
+    emitGameState();
+  };
 
   io.use(async (socket, next) => {
     try {
       const decoded = await decodeToken(socket.handshake.auth.token);
       socket.user = await resolveUser(decoded.id);
+      if (game.lobby.find((p) => p.socket.user.id === socket.user.id)) {
+        throw new Error("this user already connected");
+      }
       next();
     } catch (err) {
-      socket.disconnect(true);
+      socket.disconnect();
     }
   });
 
@@ -20,18 +47,34 @@ export function game() {
     console.log(`Player ${socket.user.username} connected`);
 
     socket.on("ready", () => {
-      player.ready = true;
-      socket.emit("lobbyStatusChange", player.ready);
+      if (!game.starting) {
+        player.ready = true;
+        socket.emit("lobbyStatusChange", player.ready);
+        if (game.lobby.filter((p) => p.ready).length === MAX_PLAYERS) {
+          game.starting = true;
+          startingTimeout = setTimeout(startGame, 2000);
+        }
+      }
     });
 
     socket.on("notReady", () => {
-      player.ready = false;
-      socket.emit("lobbyStatusChange", player.ready);
+      if (!game.starting) {
+        player.ready = false;
+        socket.emit("lobbyStatusChange", player.ready);
+      }
     });
 
     socket.on("disconnect", () => {
       console.log(`Player ${socket.user.username} disconnected`);
-      game.lobby = game.lobby.filter((s) => s.id !== socket.id);
+      if (player.ready && game.starting && !game.started) {
+        game.starting = false;
+        clearTimeout(startingTimeout);
+      }
+      game.lobby = game.lobby.filter((p) => p.socket.id !== socket.id);
+
+      if (game.started) {
+        // todo: remove from player list or not idk XD
+      }
     });
   });
 }
---
+++ utils/gameUtils.mjs
@@ -0,0 +1,52 @@
+export const MAP_SIZE = 20;
+export const BOX_COUNT = 200;
+export const EMPTY_PLACE = 0;
+export const BOX_PLACE = 1;
+export const HARD_WALL = 2;
+
+export const generateNewGame = () => {
+  const map = Array(MAP_SIZE)
+    .fill(0)
+    .map(() =>
+      Array(MAP_SIZE)
+        .fill(0)
+        .map(() => ({ type: EMPTY_PLACE, occupiedBy: null }))
+    );
+
+  for (let y = 0; y < MAP_SIZE; y++) {
+    for (let x = 0; x < MAP_SIZE; x++) {
+      if (y % 2 === 0 && x % 2 === 0) {
+        map[y][x].type = HARD_WALL;
+      }
+    }
+  }
+
+  let count = 0;
+  while (count < BOX_COUNT) {
+    const x = Math.floor(Math.random() * MAP_SIZE);
+    const y = Math.floor(Math.random() * MAP_SIZE);
+    if (map[y][x].type === EMPTY_PLACE) {
+      map[y][x].type = BOX_PLACE;
+      count++;
+    }
+  }
+
+  map[0][0].type = EMPTY_PLACE;
+  map[0][1].type = EMPTY_PLACE;
+  map[1][0].type = EMPTY_PLACE;
+
+  map[MAP_SIZE - 1][0].type = EMPTY_PLACE;
+  map[MAP_SIZE - 1][1].type = EMPTY_PLACE;
+  map[MAP_SIZE - 2][0].type = EMPTY_PLACE;
+
+  map[MAP_SIZE - 1][MAP_SIZE - 1].type = EMPTY_PLACE;
+  map[MAP_SIZE - 1][MAP_SIZE - 2].type = EMPTY_PLACE;
+  map[MAP_SIZE - 2][MAP_SIZE - 1].type = EMPTY_PLACE;
+
+  map[0][MAP_SIZE - 1].type = EMPTY_PLACE;
+  map[0][MAP_SIZE - 2].type = EMPTY_PLACE;
+  map[1][MAP_SIZE - 1].type = EMPTY_PLACE;
+
+  console.log("map", map);
+  return map;
+};
